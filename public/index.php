85cb587076ff74532c8ed119e81a326e85836b99
Tvard <i.tvard@gmail.com>
2022-09-07 14:54:41 +0300

    add sign in function

--- public/index.php
+++ public/index.php
@@ -30,13 +30,40 @@ $app->get('/', function ($request, $response) {
     return $this->get('renderer')->render($response, 'index.phtml');
 });
 
-$app->delete("/users/{id}", function ($request, $response, $agrs) use ($database, $router) {
+$app->delete("/users/{id}/cabinet", function ($request, $response, $agrs) use ($database, $router) {
     $id = $agrs['id'];
-    $users = $database->getUsers();
-    $filterUsers = collect($users)->filter(fn($user) => $user['id'] !== $id)->all();
-    $encodedUser = json_encode($filterUsers);
-    $this->get('flash')->addMessage('success', 'User has been deleted');
-    return $response->withRedirect($router->urlFor('users'))->withHeader('Set-Cookie', "users={$encodedUser}");
+    $_SESSION['users'][$id]['auth'] = null;
+    $this->get('flash')->addMessage('success', 'you are sign out');
+    return $response->withRedirect($router->urlFor('users'));
+});
+
+$app->get('/users/{id}/cabinet', function ($request, $response, $agrs) use ($database, $router) {
+    $id = $agrs['id'];
+    $params = [
+        'user' => $_SESSION['users'][$id],
+    ];
+    return $this->get('renderer')->render($response, "users/cabinet.phtml", $params);
+})->setName('cabinet');
+
+$app->get('/users/auth', function ($request, $response) {
+    $messages = $this->get('flash')->getMessages();
+    $params = [
+        'flash' => $messages,
+    ];
+    return $this->get('renderer')->render($response, "users/auth.phtml", $params);
+})->setName('auth');
+
+$app->post('/users/auth', function ($request, $response) use ($router, $database) {
+    $email = $request->getParsedBodyParam('email');
+    $user = $database->findUserViaEmail($email);
+    if ($user) {
+        $this->get('flash')->addMessage('success', 'Email Access');
+        $url = $router->urlFor('cabinet', $user);
+        $_SESSION['users'][$user['id']]['auth'] = true;
+        return $response->withRedirect($url, 302);
+    }
+    $this->get('flash')->addMessage('error', 'Email denied');
+    return $response->withRedirect('auth');
 });
 
 $app->get('/users/new', function ($request, $response) {
@@ -54,17 +81,15 @@ $app->patch('/users/{id}', function ($request, $response, $agrs) use ($router, $
     $errors = $validator->validate($data);
     if (count($errors) === 0) {
         $users = $database->getUsers();
-        $usersNew = collect($users)->map(function ($value) use ($id, $data) {
-            if ($value['id'] === $id) {
-                $value['nickname'] = $data['nickname'];
-                $value['email'] = $data['email'];
-            }
-            return $value;
-        })->all();
-        $encodedUser = json_encode($usersNew);
+        if ($users[$id]) {
+            $_SESSION['users'][$id]['nickname'] = $data['nickname'];
+            $_SESSION['users'][$id]['email'] = $data['email'];
+        }
         $this->get('flash')->addMessage('success', 'User has been updated');
         $url = $router->urlFor("users");
-        return $response->withStatus(302)->withRedirect($url)->withHeader('Set-Cookie', "users={$encodedUser}");
+        return $response
+            ->withStatus(302)
+            ->withRedirect($url);
     }
     $params = ['user' => $data, 'errors' => $errors];
     return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
@@ -74,7 +99,7 @@ $app->get('/users/{id}', function ($request, $response, $agrs) use ($database) {
     $id = $agrs['id'];
     $user = $database->findUser($id);
     if ($user) {
-        $params = ["user" => $user];
+        $params = ["user" => $user, 'id' => $id];
         return $this
             ->get('renderer')
             ->render($response, 'users/edit.phtml', $params);
@@ -85,25 +110,17 @@ $app->get('/users/{id}', function ($request, $response, $agrs) use ($database) {
         ->render($response->write("Page not find")->withStatus(404), 'users/show.phtml', $params);
 })->setName('user');
 
-$app->get('/users/{id}/edit', function ($request, $response, $agrs) use ($database) {
-    $id = $agrs['id'];
-    $user = $database->findUser($id);
-    $params = [
-        'user' => $user,
-        'errors' => []
-    ];
-    return $this
-        ->get('renderer')
-        ->render($response, "/users/edit.phtml", $params);
-})->setName('edit_user');
-
 $app->get('/users', function ($request, $response) use ($database) {
     $users = $database->getUsers();
-    $page = $request->getQueryParam('page', 1);
-    $per = $request->getQueryParam('per', 5);
-    $info = array_slice($users, ($page - 1) * $per, $per);
-    $messages = $this->get('flash')->getMessages();
-    $params = ["users" => $info, 'flash' => $messages, 'page' => $page];
+    if ($users) {
+        $page = $request->getQueryParam('page', 1);
+        $per = $request->getQueryParam('per', 5);
+        $info = array_slice($users, ($page - 1) * $per, $per);
+        $messages = $this->get('flash')->getMessages();
+        $params = ["users" => $info, 'flash' => $messages, 'page' => $page];
+        return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+    }
+    $params = ['users' => [], 'flash' => []];
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 })->setName('users');
 
@@ -112,16 +129,21 @@ $app->post('/users', function ($request, $response) use ($router, $database) {
     $validator = new App\Validator();
     $errors = $validator->validate($user);
     if (count($errors) === 0) {
-        $users = json_decode($request->getCookieParam("users", json_encode([])), true);
-        $user['id'] = uniqid();
-        $users[] = $user;
-        $encodedUser = json_encode($users);
+        $user['auth'] = null;
+        $database->save($user);
         $this->get('flash')->addMessage('success', 'Registration done');
         $url = $router->urlFor('users');
-        return $response->withRedirect($url, 302)->withHeader('Set-Cookie', "users={$encodedUser}");
+        return $response->withRedirect($url, 302);
     }
     $params = ['user' => $user, 'errors' => $errors];
     return $this->get('renderer')->render($response, "users/new.phtml", $params);
+});
+
+$app->delete("/users/{id}", function ($request, $response, $agrs) use ($database, $router) {
+    $id = $agrs['id'];
+    unset($_SESSION['users'][$id]);
+    $this->get('flash')->addMessage('success', 'User has been deleted');
+    return $response->withRedirect($router->urlFor('users'));
 });
 
 $app->run();
--- src/Database.php
+++ src/Database.php
@@ -7,35 +7,56 @@ class Database
     public function __construct()
     {
         session_start();
-        if (!array_key_exists("users", $_COOKIE)) {
-            setcookie("users", "", 0, "/");
+        if (!isset($_SESSION['users'])) {
+            $_SESSION['users'] = [];
         }
     }
 
-    public function save($user)
+    public function getUsers()
     {
-        if (!$user['nickname'] || !$user['email']) {
-            $json = json_encode($user);
-            throw new \Exception("Wrong data: {$json}");
+        if (!$_SESSION['users']) {
+            throw new \Exception("We dont have any user");
         }
-        if (!isset($user['id'])) {
-            $user['id'] = uniqid();
-        }
-    }
-
-    public function getUsers(): array
-    {
-        return json_decode($_COOKIE['users'], true);
+        return $_SESSION['users'];
     }
 
     public function findUser($id)
     {
         $users = $this->getUsers();
-        $find = collect($users)->firstWhere('id', $id);
-        if (!isset($find)) {
-            $user = json_encode($find);
-            throw new \Exception("Can't find user {$user}");
+        $user = $users[$id];
+        if (!$user) {
+            $error = json_encode($user);
+            throw new \Exception("Can't find user {$error}");
         }
-        return $find;
+        return $user;
+    }
+
+    public function save($user)
+    {
+        $id = uniqid();
+        if (!isset($_SESSION['users'][$id])) {
+            $_SESSION['users'][$id] = $user;
+        } else {
+            $error = json_encode($_SESSION['users'][$id]);
+            throw new \Exception("This user was create {$error}");
+        }
+    }
+
+    public function findUserViaEmail($email)
+    {
+        $users = $this->getUsers();
+        $id = [];
+        foreach ($users as $key => $user) {
+            if (array_key_exists('email', $user)) {
+                if ($user['email'] === $email) {
+                    $id = $key;
+                    break;
+                }
+            }
+        }
+        if ($id === []) {
+            throw new \Exception("user by email not found");
+        }
+        return ['id' => $id, 'user' => $users[$id]];
     }
 }
